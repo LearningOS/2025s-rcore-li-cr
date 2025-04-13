694ef7bd3fa92662afb7f4ea02c8946586508310
li-cr <li-cr@a.com>
2025-04-13 19:25:04 +0800

    1

---
+++ os/src/main.rs
@@ -0,0 +1,6 @@
+#![no_std]
+#![no_main]
+mod lang_items;
+
+sudo visudo
+cargo install cargo-binutils
---
+++ os/target/riscv64gc-unknown-none-elf/debug/os.d
@@ -0,0 +1,1 @@
+/mnt/os/target/riscv64gc-unknown-none-elf/debug/os: /mnt/os/src/lang_items.rs /mnt/os/src/main.rs
